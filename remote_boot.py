987b594206286da430973ad6480701b30d1b30e6
Chris Hill <chris.j.hill94@gmail.com>
2019-03-04 15:49:00 +0000

    fix error

--- remote_boot.py
+++ remote_boot.py
@@ -13,7 +13,7 @@ GPIO.setup(relay_pin, GPIO.OUT) # LED pin set as output
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
 
 while(1):
-	if GPIO.input(button) == TRUE :
+	if GPIO.input(button) == true :
 		GPIO.output(relay_pin, GPIO.HIGH)
 		time.sleep(hold_time)
 		GPIO.output(relay_pin, GPIO.LOW)
